6cb501895b957d8b62eec166d6c96c3c48ff0b13
Zak Angelle <zaknotzak@gmail.com>
2014-09-29 22:24:43 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Load all grunt tasks matching the `grunt-*` pattern
+  require('load-grunt-tasks')(grunt);
+
+  // Configs
+  grunt.config('releasePath'   , './dist');
+  grunt.config('jsOutput'      , 'main.js');
+  grunt.config('jsVendorOutput', 'vendor.js');
+  grunt.config('cssOutput'     , 'style.css');
+
+  // Load task configs
+  grunt.initConfig({
+    clean      : require('./tasks/clean.js')(grunt),
+    //browserify : require('./tasks/browserify.js')(grunt),
+    //uglify     : require('./tasks/uglify.js')(grunt),
+    copy       : require('./tasks/copy.js')(grunt),
+    //less       : require('./tasks/less.js')(grunt),
+    //jshint     : require('./tasks/jshint.js')(grunt),
+    //watch      : require('./tasks/watch.js')(grunt)
+  });
+
+  // Release
+  grunt.registerTask('build', [
+    'clean',
+    'copy'
+  ]);
+};
---
+++ app/server/main.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path    = require('path');
+
+var PORT     = process.env.PORT || 8123;
+var WEB_ROOT = path.resolve(path.join(__dirname, '../../dist'));
+
+var app = express();
+
+// Web app endpoint
+app.get('/test', function(req, res) {
+  res.end('Hello, World! From the server.');
+});
+
+// Serve static content
+app.use(express.static(WEB_ROOT));
+
+// Start server
+app.listen(PORT, function(err) {
+  if (err)
+    return console.error(err);
+  console.log('HTTP server listening on ' + PORT);
+  console.log('Web root being served from ' + WEB_ROOT);
+});
